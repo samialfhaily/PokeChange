55cb13a60aba4ce9489e408f67676b1942cc7d7e
Sami Alfhaily <sami@alfhaily.me>
2022-11-19 00:29:06 +0100

    Add project

---
+++ Sources/XchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XchangeApp.swift
+//  App
+//
+//  Created by Sami Alfhaily on 19.11.22.
+//
+
+import SwiftUI
+
+@main
+struct XchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
