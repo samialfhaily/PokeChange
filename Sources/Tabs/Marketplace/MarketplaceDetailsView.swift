66dedc8dd6a08d1643bdf1b7f156d76cfec188db
Atharva Mathapati <atharva.mathapati@tum.de>
2022-11-19 20:05:21 +0100

    Add marketplace details for each card

--- Sources/Tabs/Marketplace/MarketplaceDetailsView.swift
+++ Sources/Tabs/Marketplace/MarketplaceDetailsView.swift
@@ -7,18 +7,34 @@
 
 import SwiftUI
 
-/*
-struct Response: Codable {
-    var orders: [Order]
-}
- struct Order: Codable {
+struct ActiveOrdersResponse: Codable {
     let buy: [MasterOrder]
     let sell: [MasterOrder]
 }
- */
 
-struct MarketplaceDetailsView: View {
+final class MarketplaceCardDetailsViewModel: ObservableObject {
+    @Published var buy: [MasterOrder]?
+    @Published var sell: [MasterOrder]?
+    
+    @MainActor func fetchOrders(cardId: String) async {
+        let url = URL(string: "https://andreascs.com/api/cards/\(cardId)/orders")!
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(ActiveOrdersResponse.self, from: data)
+            self.buy = response.buy
+            self.sell = response.sell
+        } catch {
+            self.buy = []
+            self.sell = []
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct MarketplaceCardDetailsView: View {
     let card: Card
+    @StateObject private var viewModel = MarketplaceCardDetailsViewModel()
     
     var body: some View {
         VStack(spacing: 20) {
@@ -61,35 +77,58 @@ struct MarketplaceDetailsView: View {
                 Spacer(minLength: .zero)
             }
             
-            VStack(alignment: .leading, spacing: 8) {
-                HStack {
-                    Text("Buy Requests")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    
-                    Spacer(minLength: .zero)
+            Group {
+                if let buyOrders = viewModel.buy, let sellOrders = viewModel.sell {
+                    VStack(alignment: .leading, spacing: 8) {
+                        VStack(alignment: .leading) {
+                            Text("Buy Requests")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            
+                            Group {
+                                if buyOrders.isEmpty {
+                                    Text("No buy orders.")
+                                } else {
+                                    ForEach(buyOrders) { buyOrder in
+                                        Text(buyOrder.price, format: .currency(code: "USD"))
+                                    }
+                                }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                        
+                    }
+                    VStack(alignment: .leading) {
+                        Text("Sell Offers")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Group {
+                            if sellOrders.isEmpty {
+                                Text("No sell orders.")
+                            } else {
+                                ForEach(sellOrders) { sellOrder in
+                                    Text(sellOrder.price, format: .currency(code: "USD"))
+                                }
+                            }
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    }
+                } else {
+                    ProgressView("Loading...")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .task {
+                            await viewModel.fetchOrders(cardId: card.id)
+                        }
                 }
-                
             }
-            
-            Spacer()
-            
-            VStack(alignment: .leading, spacing: 8) {
-                HStack {
-                    Text("Sell Offers")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Spacer(minLength: .zero)
-                }
-                
-            }
-            
-            Spacer(minLength: .zero)
+            .animation(.default, value: viewModel.buy)
+            .animation(.default, value: viewModel.sell)
             
             HStack (spacing: 20) {
-                Button(action: {
-                    ()
-                }) {
+                Button {
+                    
+                } label: {
                     Text("Sell")
                         .fontWeight(.bold)
                         .foregroundColor(.bbBlack)
@@ -97,40 +136,37 @@ struct MarketplaceDetailsView: View {
                         .padding()
                         .background(
                             RoundedRectangle(cornerRadius: 10)
-                                                .stroke(lineWidth: 2)
-                                                .background(Color.green))
+                                .stroke(lineWidth: 2)
+                                .background(Color.green))
                         .cornerRadius(10)
-
                 }
                 
-                Button(action: {
-                    ()
-                }) {
+                Button {
+                } label: {
                     Text("Buy")
-                        //.font(.system(size: CGFloat))
                         .fontWeight(.bold)
                         .foregroundColor(.bbBlack)
                         .frame(maxWidth: .infinity)
                         .padding()
                         .background(
                             RoundedRectangle(cornerRadius: 10)
-                                                .stroke(lineWidth: 2)
-                                                .background(Color.bbRed))
+                                .stroke(lineWidth: 2)
+                                .background(Color.bbRed))
                         .cornerRadius(10)
                 }
-                
             }
+            
             Spacer(minLength: .zero)
         }
-         .padding(20)
-         .navigationTitle(Text("Match Details"))
-         .navigationBarTitleDisplayMode(.inline)
+        .padding(20)
+        .navigationTitle(Text("Card Shop"))
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
-struct MarketplaceDetailsView_Previews: PreviewProvider {
+struct MarketplaceCardDetailsView_Previews: PreviewProvider {
     static let card = Card(id: "abc", name: "Pikachu", rarity: .amazingRare, imageUrl: URL(string: "https://images.pokemontcg.io/xy1/1.png")!)
     static var previews: some View {
-        MarketplaceDetailsView(card: card)
+        MarketplaceCardDetailsView(card: card)
     }
 }
